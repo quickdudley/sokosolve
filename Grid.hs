2dc33d7104fcd13c20214454cb5fb6436ff85d9c
Jeremy List <quick.dudley@gmail.com>
2015-09-04 22:33:29 +0800

    Internal representation for board state

---
+++ Grid.hs
@@ -0,0 +1,99 @@
+module Grid (
+  Grid(..),
+  Direction(..),
+  Tile(..),
+  directions,
+  step,
+  lookupGrid,
+  readGrid,
+  showGrid
+ ) where
+
+import Data.Array
+import qualified Data.Set as S
+import Data.List
+
+data Grid = Grid {
+  gridPlayer :: (Int,Int),
+  gridWalls :: Array (Int,Int) Bool,
+  gridBoxes :: S.Set (Int,Int),
+  gridTargets :: S.Set (Int,Int)
+ }
+
+data Direction = North | East | South | West deriving (Ord,Eq)
+data Tile = Wall | Clear | Box | Target | Player | PT | BT deriving (Eq)
+
+directions = [North,East,South,West]
+
+step North (x,y) = (x,y-1)
+step East (x,y) = (x+1,y)
+step South (x,y) = (x,y+1)
+step West (x,y) = (x-1,y)
+
+lookupGrid l g
+ | not $ inRange (bounds $ gridWalls g) l = Wall
+ | gridWalls g ! l = Wall
+ | S.member l (gridTargets g) = case () of
+    () | gridPlayer g == l -> PT
+    () | S.member l (gridBoxes g) -> BT
+    () | otherwise -> Target
+ | S.member l (gridBoxes g) = Box
+ | gridPlayer g == l = Player
+ | otherwise = Clear
+
+applyStep d g = let
+  p = gridPlayer g
+  p' = step d p
+  b' = step d p'
+  pushBox = Just $ g {
+    gridPlayer = p',
+    gridBoxes = S.insert b' $ S.delete p' $ gridBoxes g
+   }
+  in case (lookupGrid p' g, lookupGrid b' g) of
+    (Clear,_) -> Just $ g {gridPlayer = p'}
+    (Box,Clear) -> pushBox
+    (Box,Target) -> pushBox
+    (BT,Clear) -> pushBox
+    (BT,Target) -> pushBox
+    _ -> Nothing
+
+{-
+ - Input format:
+ - * : Wall
+ - . or space : Clear
+ - p : Player
+ - # : Box
+ - _ : Target
+ - + : Box on target
+ - P : Player on target
+-}
+readGrid t = let
+  l = lines t
+  h = length l
+  w = maximum $ map length l
+  i = do
+    (y,r) <- zip [0..] l
+    (x,c) <- zip [0..] r
+    return ((x,y),c)
+  in Grid {
+    gridPlayer = case filter ((`elem` "pP") . snd) i of
+      [] -> (-1,-1)
+      ((p,_):_) -> p,
+    gridWalls = array ((0,0),(w,h)) [(l,c == '*') | (l,c) <- i],
+    gridBoxes = S.fromList $ map fst $ filter ((`elem` "#+") . snd) i,
+    gridTargets = S.fromList $ map fst $ filter ((`elem` "_+P") . snd) i
+   }
+
+showGrid g = unlines $ map (map gc) pts where
+  pts = let
+    ((x',y'),(w,h)) = bounds (gridWalls g)
+    in map (\y -> map (\x -> (x,y)) [x' .. w]) [y' .. h]
+  gc l = case lookupGrid l g of
+    Wall -> '*'
+    Clear -> '.'
+    Player -> 'p'
+    Box -> '#'
+    Target -> '_'
+    BT -> '+'
+    PT -> 'P'
+
