2db09c58e68ff0dd57b0a1a3ffcf6fae035029bb
Jeremy List <quick.dudley@gmail.com>
2015-09-04 16:59:35 +0800

    Initial commit: skew heap for priority queue

---
+++ Data/Heap.hs
@@ -0,0 +1,39 @@
+module Data.Heap (
+  Heap,
+  singleton,
+  pop,
+  fromList
+ ) where
+
+import Data.Monoid (Monoid(..),(<>))
+
+data Heap p a = Empty | Heap p a (Heap p a) (Heap p a) deriving Show
+
+instance (Ord p) => Monoid (Heap p a) where
+  mempty = Empty
+  mappend heap1@(Heap p1 a1 l1 r1) heap2@(Heap p2 a2 l2 r2)
+    | p1 <= p2 = Heap p1 a1 (heap2 <> r1) l1
+    | otherwise = Heap p2 a2 (heap1 <> r2) l2
+  mappend heap Empty = heap
+  mappend Empty heap = heap
+
+instance Functor (Heap p) where
+  fmap _ Empty = Empty
+  fmap f (Heap p a l r) = Heap p (f a) (fmap f l) (fmap f r)
+
+singleton :: p -> a -> Heap p a
+singleton p a = Heap p a Empty Empty
+
+pop :: (Ord p) => Heap p a -> Maybe (p,a,Heap p a)
+pop Empty = Nothing
+pop (Heap p a l r) = Just (p,a,l <> r)
+
+fromList :: (Ord p) => [(p,a)] -> Heap p a
+fromList = tf . map (uncurry singleton) where
+  tf [] = Empty
+  tf [x] = x
+  tf l = tf (mp l)
+  mp [] = []
+  mp [x] = [x]
+  mp (a:b:r) = (a <> b) : mp r
+
