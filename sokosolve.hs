d34ab3c7f34a8f50a4051203b8c63e38f5b9176e
Jeremy List <quick.dudley@gmail.com>
2015-09-06 19:00:57 +0800

    Add main function

---
+++ sokosolve.hs
@@ -0,0 +1,29 @@
+import Grid
+import Planning
+
+import System.IO
+import System.Environment
+import Data.List (intercalate)
+import Control.Monad
+
+main = do
+  args <- getArgs
+  case args of
+    [] -> mapM_ (hPutStrLn stderr) [
+      "Usage: sokosolve <filename>",
+      "File format: ascii; newlines: Unix style",
+      " * : Wall",
+      " . or space : Clear",
+      " p : Player",
+      " # : Box",
+      " _ : Target",
+      " + : Box on target",
+      " P : Player on target",
+      "Output: UTF-8 to stdout"
+     ]
+    (fn:_) -> do
+      g <- fmap readGrid $ readFile fn
+      case solve g of
+        Nothing -> putStrLn "No solution"
+        Just s -> putStrLn $ intercalate "," $ map show s
+
